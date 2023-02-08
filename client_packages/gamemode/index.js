73148d7900b779d22fbc7c7c8080e2cc069521b1
Benj98 <38333326+Benj98@users.noreply.github.com>
2023-02-08 18:05:04 +0000

    Added script to enter nearest door of vehicle.

--- client_packages/gamemode/index.js
+++ client_packages/gamemode/index.js
@@ -2,13 +2,10 @@ require('./gamemode/advancedchat');
 require('./gamemode/auth');
 require('./gamemode/damage');
 require('./gamemode/death');
+require('./gamemode/seat');
 require('./gamemode/includes/rage-rpc.min.js')
 const rpc = require('./gamemode/includes/rage-rpc.min.js');
 
 mp.events.add('client:playerJoin', (player) => {
     mp.events.call('showLogin', player);
-})
-
-rpc.register('test', () => {
-    mp.gui.chat.push("Hello from rage-rpc.")
 })
