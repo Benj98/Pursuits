cdc8bafca216d5d2d07392a97d3b083812d10869
Benj98 <38333326+Benj98@users.noreply.github.com>
2023-02-09 09:43:16 +0000

    Update index.js

--- packages/core/damage/index.js
+++ packages/core/damage/index.js
@@ -8,16 +8,12 @@ fs.readFile('./weaponDamages.json', 'utf-8', (err, data) => {
 
 
 mp.events.add('playerAttack', (player, sourceEntity, targetEntity, targetPlayer, weapon, boneIndex, damage) => {
-    player.outputChatBox(`[DEBUG] Hit player: ${targetPlayer.name} + Bone hit: ${boneIndex} + Damage dealt: ${damage} + Weapon: ${weapon}`);
-    targetPlayer.outputChatBox(`[DEBUG] Hit by: ${player.name} + Bone hit: ${boneIndex} + Damage received: ${damage} + Weapon: ${weapon}`);
-    
+    player.outputChatBox(`[DEBUG] Hit player: ${targetPlayer.name} | Bone hit: ${boneIndex} | Damage dealt: ${damage} | Weapon: ${weapon}`);
+    targetPlayer.outputChatBox(`[DEBUG] Hit by: ${player.name} | Bone hit: ${boneIndex} | Damage received: ${damage} | Weapon: ${weapon}`);
+
+    console.log(`${sourceEntity} + ${targetEntity}`);    
 
     console.log(`${targetPlayer.name} was hit by ${player.name} for ${damage} damage.`);
-    // if(weaponDamages.pistols[0].hasOwnProperty(weapon)) {
-    //     targetPlayer.health = targetPlayer.health - weaponDamages[weapon];
-    // } else {
-    //     targetPlayer.health = targetPlayer.health - 10;
-    // }
 })
 
 mp.events.add('shotFired', (player, weapon) => {
@@ -26,5 +22,4 @@ mp.events.add('shotFired', (player, weapon) => {
     } else {
         player.outputChatBox('No custom damages. :(');
     }
-
 })
