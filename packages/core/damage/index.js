9bdb0f9ddaf4e4de3c6e1b8247f79bc78e196465
Benj98 <38333326+Benj98@users.noreply.github.com>
2023-02-09 08:40:40 +0000

    Added death reasons, removed some debug stuff.

--- packages/core/damage/index.js
+++ packages/core/damage/index.js
@@ -9,17 +9,17 @@ fs.readFile('./weaponDamages.json', 'utf-8', (err, data) => {
 console.log(weaponDamages);
 
 mp.events.add('playerAttack', (player, sourceEntity, targetEntity, targetPlayer, weapon, boneIndex, damage) => {
+    player.outputChatBox(`${targetPlayer.name}`)
     
-    if(weaponDamages.pistols[0].hasOwnProperty(weapon)) {
-        targetPlayer.health = targetPlayer.health - weaponDamages[weapon];
-    } else {
-        targetPlayer.health = targetPlayer.health - 10;
-    }
+    // if(weaponDamages.pistols[0].hasOwnProperty(weapon)) {
+    //     targetPlayer.health = targetPlayer.health - weaponDamages[weapon];
+    // } else {
+    //     targetPlayer.health = targetPlayer.health - 10;
+    // }
     player.outputChatBox(`${targetEntity} + ${sourceEntity} + ${boneIndex} + ${damage}`);
 })
 
 mp.events.add('shotFired', (player, weapon) => {
-    player.outputChatBox(weapon + ` Server`);
     if(weaponDamages.pistols.hasOwnProperty(weapon)) {
         player.outputChatBox('Custom damages!');
     } else {
--- packages/core/death/index.js
+++ packages/core/death/index.js
@@ -6,3 +6,7 @@ mp.events.add('server:PlayerRespawnFromDeath', (player) => {
     player.outputChatBox("Respawning...");
     player.spawn(player.position);
 })
+
+mp.events.add('killedByPlayer', (player, killer, weapon) => {
+    killer.outputChatBox(`You killed ${player.name} with a ${weapon}.`);
+})
